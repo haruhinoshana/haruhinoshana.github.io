892611c93afade0390771262f0213a94788ca6b0
rj <rolandjay.jtech@gmail.com>
2016-06-23 16:38:30 +0800

    123123

--- public/js/about/about.js
+++ public/js/about/about.js
@@ -5,5 +5,19 @@ $(window).ready(function(e){
         $(this).css('width', $(this).attr('aria-valuetransitiongoal')+'%');
       });
 });
-
+			// Show or hide the sticky footer button
+			$(window).scroll(function() {
+				if ($(this).scrollTop() > 200) {
+					$('.go-top').fadeIn(200);
+				} else {
+					$('.go-top').fadeOut(200);
+				}
+			});
+			
+			// Animate the scroll to top
+			$('.go-top').click(function(event) {
+				event.preventDefault();
+				
+				$('html, body').animate({scrollTop: 0}, 300);
+			});
 });
